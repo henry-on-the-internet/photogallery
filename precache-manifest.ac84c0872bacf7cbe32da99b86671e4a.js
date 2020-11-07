fa3ccd7908ffade0e5d879d2d2e507320cded669
narrator-fiction <73651440+narrator-fiction@users.noreply.github.com>
2020-11-07 14:18:52 +0800

    Updates

---
+++ precache-manifest.ac84c0872bacf7cbe32da99b86671e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d0fd4a5aa6ffac2bbde56e930775e0",
+    "url": "/photogallery/index.html"
+  },
+  {
+    "revision": "dcef9a24ff3a0b3d8216",
+    "url": "/photogallery/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "eff8f50f9f1169297c64",
+    "url": "/photogallery/static/js/2.f2b52059.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/photogallery/static/js/2.f2b52059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcef9a24ff3a0b3d8216",
+    "url": "/photogallery/static/js/main.5bc1b2d5.chunk.js"
+  },
+  {
+    "revision": "cd37dbdd71b35f7fc175",
+    "url": "/photogallery/static/js/runtime-main.820ffab6.js"
+  }
+]);
